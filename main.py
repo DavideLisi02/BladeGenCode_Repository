36cc60c796cbf5805a6db7c82607862c0182d4b6
DavideLisi02 <davide.lisi@epfl.ch>
2024-11-13 13:59:01 +0100

    LUS MODIFIED

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ Parameters = ParametrizationSettings(
         print_conversion_output = True,
         tau =  [0.5, 1],
         w1 = 5,
-        par_name = f"LUM")
+        par_name = f"LUS")
 
 Curve = Bezier(Parameters)
 
 Geometry_02 = Geometry(Parameters,
                         Curve,
-                        defaultfilePath ='defaultBGI\\LUM.bgi', # Path of the starting bgi model to modify
-                        output_jsonPath = 'modifiedJSON\\MODLUM_test.json',
-                        output_bgiPath = 'modifiedBGI\\MODLUM_test.bgi',
-                        output_unmodified_bgiPath = 'unmodifiedBGI\\UNMODLUM_test.bgi',
-                        output_bgdPath = 'modifiedBGD\\MODLUM_test.bgd',
-                        output_unmodified_bgdPath = 'unmodifiedBGD\\UNMODLUM_test.bgd',
+                        defaultfilePath = 'defaultBGI\\LUS.bgi', # Path of the starting bgi model to modify
+                        output_jsonPath = 'modifiedJSON\\MODLUS_test.json',
+                        output_bgiPath = 'modifiedBGI\\MODLUS_test.bgi',
+                        output_unmodified_bgiPath = 'unmodifiedBGI\\UNMODLUS_test.bgi',
+                        output_bgdPath = 'modifiedBGD\\MODLUS_test.bgd',
+                        output_unmodified_bgdPath = 'unmodifiedBGD\\UNMODLUS_test.bgd',
                         std_ANSYS_Folder = "c:\\Program Files\\ANSYS Inc")
 
 #Geometry_02.create_unmodified_json_geometry()
