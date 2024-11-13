922c12f93b647ecac5ab3888cc39a98a2c7e8fbf
DavideLisi02 <davide.lisi@epfl.ch>
2024-11-13 13:54:50 +0100

    Single test updated

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from Bezier import *
 from Parameters import *
 from stateFileModifier import *
 
-Parameters = ParametrizationSettings()
+Parameters = ParametrizationSettings(
+        beta_in = 24.008,
+        beta_out = 35.15,
+        print_conversion_output = True,
+        tau =  [0.5, 1],
+        w1 = 5,
+        par_name = f"LUM")
+
 Curve = Bezier(Parameters)
 
 Geometry_02 = Geometry(Parameters,
-                      Curve,
-                      defaultfilePath ='defaultBGI\\geometry02.bgi',
-                      output_jsonPath = 'modifiedJSON\\geometry02.json')
+                        Curve,
+                        defaultfilePath ='defaultBGI\\LUM.bgi', # Path of the starting bgi model to modify
+                        output_jsonPath = 'modifiedJSON\\MODLUM_test.json',
+                        output_bgiPath = 'modifiedBGI\\MODLUM_test.bgi',
+                        output_unmodified_bgiPath = 'unmodifiedBGI\\UNMODLUM_test.bgi',
+                        output_bgdPath = 'modifiedBGD\\MODLUM_test.bgd',
+                        output_unmodified_bgdPath = 'unmodifiedBGD\\UNMODLUM_test.bgd',
+                        std_ANSYS_Folder = "c:\\Program Files\\ANSYS Inc")
 
 #Geometry_02.create_unmodified_json_geometry()
 Geometry_02.create_modified_geometry()
