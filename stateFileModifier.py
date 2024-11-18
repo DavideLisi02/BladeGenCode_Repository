61c01adeaf457d9e1ee3edaae703f90d15aaa0dc
DavideLisi02 <davide.lisi@epfl.ch>
2024-11-18 13:57:17 +0100

    bgi bgd test

--- stateFileModifier.py
+++ stateFileModifier.py
@@ -414,6 +414,9 @@ class Geometry:
         blade_batch_command = f'BladeBatch {output_bgiPath} {output_bgdPath}'
         print(f"Converting using Blade Batch: {blade_batch_command}")
         
+        full_command = f"{folder_command} && {blade_batch_command}"
+        print(f"Full command: {full_command}")
+
         #running full command
         run_command(f"{folder_command} && {blade_batch_command}")
 
