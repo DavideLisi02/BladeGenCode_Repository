8944cb36a32439920f13cc9cb23a1a79e0ae18e9
DavideLisi02 <davide.lisi@epfl.ch>
2024-11-18 17:12:38 +0100

    Moving bds for simualtions update

--- runMultipleSimulation.py
+++ runMultipleSimulation.py
@@ -13,13 +13,15 @@ from simulationParameters import *
 
 #! Folder Management Settings
 Project_Name = "Database_Test_2"
-Project_Folder = "D:\\Davide"
+Project_Folder = "D:\\Davide" # Absolute path of the project folder created for the geometries
 number_of_channels = 9
 std_ANSYS_Folder_settings = "C:\\Program Files\\ANSYS Inc\\v242"
 
 ####################################################################################
 ####################################################################################
 
+Folder_management.copy_bgd_for_simulation(f"{Project_Folder}\\{Project_Name}\\{Folder_management.output_bgdfolder_settings}", f"{package_path}\\CFXsetup_files\\geometry_turbocompressor_database")
+'''
 simulation_definition = create_simulation_def(ansys_path = std_ANSYS_Folder_settings,
                                             simulation_path = f"{Project_Folder}\\{Project_Name}\\{Folder_management.output_simulation_folder}",
                                             case_name = 'LUS_1',
@@ -27,3 +29,4 @@ simulation_definition = create_simulation_def(ansys_path = std_ANSYS_Folder_sett
                                             n_channels = number_of_channels)
 
 CFXbatch.run_CFXbatch(simulation_definition)
+'''
